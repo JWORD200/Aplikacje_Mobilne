b31e458f3c479048b718a236c8522bbac7d8feaa
JWORD200 <jan.tomczak@zsegw.com>
2024-09-25 21:07:52 +0200

    Bitmap

--- Bitmap/app/src/main/java/com/example/bitmap/MainActivity.java
+++ Bitmap/app/src/main/java/com/example/bitmap/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.bitmap;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private Bitmap bitmap;
     private int rotationAngle = 0;
     private boolean isImageVisible = true;
+    private boolean isDarkened = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         imageView = findViewById(R.id.imageView);
-        imageSizeLabel = findViewById(R.id.imgSizeLabel);
+        imageSizeLabel = findViewById(R.id.imageDimensions);
 
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_foreground);
         imageView.setImageBitmap(bitmap);
@@ -34,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         Button buttonHide = findViewById(R.id.btnHide);
         Button buttonRotateLeft = findViewById(R.id.btnRotateLeft);
         Button buttonRotateRight = findViewById(R.id.btnRotateRight);
+        Button buttonSetDark = findViewById(R.id.btnSetDark);
 
         buttonShow.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,12 +70,29 @@ public class MainActivity extends AppCompatActivity {
                 imageView.setRotation(rotationAngle);
             }
         });
-    }
 
-    private void updateImageSizeLabel() {
-        int width = bitmap.getWidth();
-        int height = bitmap.getHeight();
-        String sizeText = "Image Size: " + width + "x" + height;
-        imageSizeLabel.setText(sizeText);
+
+        buttonSetDark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isDarkened) {
+                    ColorMatrix colorMatrix = new ColorMatrix();
+                    colorMatrix.setScale(0.5f, 0.5f, 0.5f, 1.0f); // Skala 0.5f dla RGB przyciemnia obraz
+                    imageView.setColorFilter(new ColorMatrixColorFilter(colorMatrix));
+                    buttonSetDark.setText("Przywróć obrazek");
+                    isDarkened = true;
+                } else {
+                    imageView.clearColorFilter();
+                    buttonSetDark.setText("Przyciemnij obrazek");
+                    isDarkened = false;
+                }
+            }
+        });
+    }
+        private void updateImageSizeLabel() {
+            int width = bitmap.getWidth();
+            int height = bitmap.getHeight();
+            String sizeText = "Image Size: " + width + "x" + height;
+            imageSizeLabel.setText(sizeText);
     }
 }
